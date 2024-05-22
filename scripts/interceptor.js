abf7b144404c1b194cb0031ea346e228504b30ee
lark.hzy@gmail.com <hzy@zjca.com.cn>
2024-05-22 10:41:57 +0800

    update.

---
+++ scripts/interceptor.js
@@ -0,0 +1,17 @@
+// interceptor.js
+document.addEventListener('DOMContentLoaded', function () {
+    // 你需要执行的全局逻辑
+    checkUserLoginStatus();
+    // console.log('Interceptor loaded on all pages');
+});
+
+/**
+ * 检查用户登录状态
+ */
+function checkUserLoginStatus() {
+    const loggedIn = sessionStorage.getItem('loggedIn');
+    if (!loggedIn) {
+        // 可以重定向到登录页面
+        window.location.href = '/';
+    }
+}
